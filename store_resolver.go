d090a510f72bbb45442f6332323234c0e05cdab0
Ian Hansen <ian@supershabam.com>
2014-09-10 22:47:33 -0400

    ah yeah

--- store_resolver.go
+++ store_resolver.go
@@ -12,5 +12,20 @@ type StoreResolver struct {
 
 // Resolve turns a target string into a list of series
 func (r StoreResolver) Resolve(target Target, from, until time.Time) ([]Series, error) {
-	return []Series{}, fmt.Errorf("NOT IMPLEMENTED")
+	if target.IsFunction {
+		if fn, ok := r.Functions[target.Name]; ok {
+			return fn(r, target.Args, from, until)
+		}
+		return []Series{}, fmt.Errorf("function %s not found", target.Name)
+	}
+	datapoints, err := r.Store.Get(Key(target.Pattern), from, until)
+	if err != nil {
+		return []Series{}, err
+	}
+	return []Series{
+		Series{
+			Name:       target.Pattern,
+			Datapoints: datapoints,
+		},
+	}, nil
 }
